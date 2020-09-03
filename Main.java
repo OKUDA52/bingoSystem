b0d53d717f68dc758ad084d99c4dc7e1f888449e
okuda <muhen101@gmail.com>
2020-09-03 12:50:16 +0900

    Main 引数変更

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 //Input how many player as args[0]
-try{
-        BingoGame bg=new BingoGame(Integer.parseInt(args[0]));
+        int player=1;
+        if(args.length!=0){
+            try{
+                player=Integer.parseInt(args[0]);
+            }catch(NumberFormatException e){
+                System.out.println("プレイヤー数を整数で入力してください");
+            }
+        }
+
+        BingoGame bg=new BingoGame(player);
         bg.gameStart();
-    }catch(NumberFormatException e){
-System.out.println("プレイヤー数を入力してください");
-}
- 
+        
         
     }
 
